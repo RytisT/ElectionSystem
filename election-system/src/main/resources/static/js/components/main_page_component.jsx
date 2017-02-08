d50fecd3fc458748d6406256484ea9e42d9a9f26
Diktas <g.simanelis@gmail.com>
2017-02-08 19:36:33 +0200

    react main page update

--- election-system/src/main/resources/static/js/components/main_page_component.jsx
+++ election-system/src/main/resources/static/js/components/main_page_component.jsx
@@ -6,6 +6,15 @@ var MainPageComponent = React.createClass( {
                 <h1> Election system </h1>
                 <button className="btn btn-success" onClick={this.props.onAdminClick}  >Admin</button>
                 <button className="btn btn-success" onClick={this.props.onRepresentativesClick}  >Representatives</button>
+                <div> <p> Resultatai </p> </div>
+                <div>
+                    <ul>
+                        <li><a>Rinkimų apylinkių rezultatai</a></li>
+                        <li><a>Rinkimų apygardų rezultatai</a></li>
+                        <li><a>Daugiamandatė rinkimų apygardos rezultatai</a></li>
+                        <li><a>Konsoliduoti rinkimų apygardos rezultatai</a></li>
+                    </ul>
+                </div>
             </div>
         )
     }
