6e5dd1472fb52ad17584e2f85921e0d5b2c96c6f
Diktas <g.simanelis@gmail.com>
2017-02-08 20:35:26 +0200

    react main page update
    some design added

--- election-system/src/main/resources/static/js/components/main_page_component.jsx
+++ election-system/src/main/resources/static/js/components/main_page_component.jsx
@@ -1,19 +1,32 @@
+var styles = {
+  results: {
+    margin: '20 0 0 5',
+    fontSize: '20px'
+  },
+  space: {
+      margin: '0 20 0 0'
+  },
+  cursor: {
+      cursor: 'pointer'
+  }
+};
+
 var MainPageComponent = React.createClass( {
     render: function() {
 
         return (
             <div className="container">
                 <h1> Election system </h1>
-                <button className="btn btn-success" onClick={this.props.onAdminClick}  >Admin</button>
+                <button className="btn btn-success"  style={styles.space} onClick={this.props.onAdminClick}  >Admin</button>
                 <button className="btn btn-success" onClick={this.props.onRepresentativesClick}  >Representatives</button>
-                <div> <p> Resultatai </p> </div>
+                <div> <p style={styles.results}> Rezultatai </p> </div>
                 <div>
                     <ul>
-                        <li><a>Rinkimų apylinkių rezultatai</a></li>
-                        <li><a>Rinkimų apygardų rezultatai</a></li>
-                        <li><a>Vienmandatės rinkimų apygardos rezultatai</a></li>
-                        <li><a>Daugiamandatės rinkimų apygardos rezultatai</a></li>
-                        <li><a>Konsoliduoti rinkimų apygardos rezultatai</a></li>
+                        <li><a style={styles.cursor}>Rinkimų apylinkių rezultatai</a></li>
+                        <li><a style={styles.cursor}>Rinkimų apygardų rezultatai</a></li>
+                        <li><a style={styles.cursor}>Vienmandatės rinkimų apygardos rezultatai</a></li>
+                        <li><a style={styles.cursor}>Daugiamandatės rinkimų apygardos rezultatai</a></li>
+                        <li><a style={styles.cursor}>Konsoliduoti rinkimų apygardos rezultatai</a></li>
                     </ul>
                 </div>
             </div>
