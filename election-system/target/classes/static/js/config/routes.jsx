dea11a0442e2f8fb1ff652a5d637eeb6aec294c7
Diktas <g.simanelis@gmail.com>
2017-02-15 11:54:07 +0200

    .

--- election-system/target/classes/static/js/config/routes.jsx
+++ election-system/target/classes/static/js/config/routes.jsx
@@ -11,6 +11,9 @@ var routes = (
         <Route path="/stuff" component={Constituencies} />
         <Route path="/district/:distId" component={DistrictContainer} />
         <Route path="/Contacts" component={Constituencies} />
+        <Route path="/admin/candidates" component={CandidatesContainer} />
+        <Route path="/admin/candidates/add-candidate" component={AddCandidateContainer} />
+        <Route path="/admin/candidates/edit/:candidateId" component={EditCandidateContainer} />
     </Route>
     </Router>
     </Router>
