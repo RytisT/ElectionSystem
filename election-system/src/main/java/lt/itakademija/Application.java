3f0dc02f0f8c54f96fa0b78b1c3f3a9fb5b38989
JaroslavB <j.burinskij@gmail.com>
2017-02-17 11:56:32 +0200

    no message

--- election-system/src/main/java/lt/itakademija/Application.java
+++ election-system/src/main/java/lt/itakademija/Application.java
@@ -1,11 +1,26 @@
 package lt.itakademija;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
+import lt.itakademija.uploadCSV.StorageProperties;
+import lt.itakademija.uploadCSV.StorageService;
+
+@EnableConfigurationProperties(StorageProperties.class)
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+	
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+		};
+	}
 }
---
+++ election-system/src/main/java/lt/itakademija/uploadCSV/FileUploadController.java
@@ -0,0 +1,69 @@
+package lt.itakademija.uploadCSV;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.stream.Collectors;
+
+@Controller
+public class FileUploadController {
+
+    private final StorageService storageService;
+
+    @Autowired
+    public FileUploadController(StorageService storageService) {
+        this.storageService = storageService;
+    }
+
+    @GetMapping(value={"/uploadForm"})
+    public String listUploadedFiles(Model model) throws IOException {
+
+        model.addAttribute("files", storageService
+                .loadAll()
+                .map(path ->
+                        MvcUriComponentsBuilder
+                                .fromMethodName(FileUploadController.class, "serveFile", path.getFileName().toString())
+                                .build().toString())
+                .collect(Collectors.toList()));
+
+        return "uploadForm.html";
+    }
+
+    @GetMapping("/uploadForm/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) throws IOException, SQLException {
+
+        Resource file = storageService.loadAsResource(filename);
+        return ResponseEntity
+                .ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\""+file.getFilename()+"\"")
+                .body(file);
+    }
+
+    @PostMapping("/uploadForm")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) throws SQLException {
+
+        storageService.store(file);
+        redirectAttributes.addFlashAttribute("message",
+                "Failas sekmingai ikeltas " + file.getOriginalFilename() + "!");
+
+        return "redirect:/uploadForm";
+    }
+
+    @ExceptionHandler(StorageFileNotFoundException.class)
+    public ResponseEntity handleStorageFileNotFound(StorageFileNotFoundException exc) {
+        return ResponseEntity.notFound().build();
+    }
+
+}
