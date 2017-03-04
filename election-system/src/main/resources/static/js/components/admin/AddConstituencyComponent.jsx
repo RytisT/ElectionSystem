6f9e8af884ba25af6483ed95388d8c81d90a9ecd
Diktas <g.simanelis@gmail.com>
2017-03-04 12:10:31 +0200

    Add Constituency Validation

--- election-system/src/main/resources/static/js/components/admin/AddConstituencyComponent.jsx
+++ election-system/src/main/resources/static/js/components/admin/AddConstituencyComponent.jsx
@@ -1,62 +1,75 @@
-var AddConstituencyComponent = React.createClass({
+var AddConstituencyComponent = React.createClass( {
 
-    getInitialState: function () {
+    getInitialState: function() {
         return {
             adding: false
         };
     },
 
-    changeAddingState(){
-        if (this.state.adding) {
-            this.setState({adding: false});
+    changeAddingState() {
+        if ( this.state.adding ) {
+            this.setState( { adding: false });
         } else {
-            this.setState({adding: true});
+            this.setState( { adding: true });
         }
     },
 
-    handleSubmitConstituency: function () {
-        if (this.state.adding) {
+    handleSubmitConstituency: function() {
+        if ( this.state.adding ) {
             return (
                 <div className=" well col-md-10  col-md-offset-1">
                     <form>
                         <table>
                             <tbody>
-                            <tr>
-                                <td className="col-md-10">
-                                    <input id="Constituency name" className="form-control" placeholder="Apygardos Pavadinimas"
-                                           value={this.props.constituency.title}
-                                           onChange={this.props.onFieldChange('title')} type="text"/>
-                                </td>
-                                <td className="col-md-2">
-                                    <button id="Submit constituency" className="btn btn-block btn-success" type="submit"
-                                            onClick={(event) => {
-                                                this.props.onSubmitConst(this.props.constituency);
-                                                this.changeAddingState();
-                                            }}>Pridėti
+                                <tr>
+                                    <td className="col-md-10">
+                                        <input id="ConstituencyName" className="form-control" placeholder="Apygardos Pavadinimas"
+                                            maxLength="30" value={this.props.constituency.title}
+                                            onChange={this.props.onFieldChange( 'title' )} type="text" />
+                                    </td>
+                                    <td className="col-md-2">
+                                        <button id="Submit constituency" className="btn btn-block btn-success" type="submit"
+                                            onClick={( event ) => {
+
+                                                var val = $( "#ConstituencyName" ).val();
+                                                var matches = val.match( ".*([a-zA-ZąčęėįšųūžĄČĘĖĮŠŲŪŽ„“]$)" );
+                                                if ( matches != null ) {
+                                                    $( '#DistrictTitleValidation' ).hide( "slow" );
+                                                    this.props.onSubmitConst( this.props.constituency );
+                                                    this.changeAddingState();
+                                                }
+                                                else {
+                                                    $( '#DistrictTitleValidation' ).hide( "slow" );
+                                                    $( '#DistrictTitleValidation' ).show( "slow" )
+                                                }
+
+                                            } }>Pridėti
                                     </button>
-                                </td>
-                                <td>
-                                    <button id="Cancel submitting" className="btn btn-block btn-danger" type="submit"
+                                    </td>
+                                    <td>
+                                        <button id="Cancel submitting" className="btn btn-block btn-danger" type="submit"
                                             onClick={this.changeAddingState}>Atšaukti
                                     </button>
-                                </td>
-                            </tr>
+                                    </td>
+                                </tr>
                             </tbody>
                         </table>
                     </form>
+                     <div id="DistrictTitleValidation" className="validationForm">
+                                    <span>Naudojami netinkami simboliai arba neužpildytas laukas.</span></div>               
                 </div>
             )
         } else {
             return (
                 <div>
                     <button id="Add Constituency" className="btn btn-block btn-success" type="submit"
-                            onClick={this.changeAddingState}>Pridėti apygardą
+                        onClick={this.changeAddingState}>Pridėti apygardą
                     </button>
-                </div>)
+                </div> )
         }
     },
 
-    render: function () {
+    render: function() {
         return (
             <div>
                 {this.handleSubmitConstituency()}
