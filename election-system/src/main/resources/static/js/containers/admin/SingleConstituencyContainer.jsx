f5be1617f7496740c082c6fc4d78c1594b479b9d
Diktas <g.simanelis@gmail.com>
2017-03-06 11:37:30 +0200

    Update on validation

--- election-system/src/main/resources/static/js/containers/admin/SingleConstituencyContainer.jsx
+++ election-system/src/main/resources/static/js/containers/admin/SingleConstituencyContainer.jsx
@@ -1,6 +1,6 @@
-var SingleConstituencyContainer = React.createClass({
+var SingleConstituencyContainer = React.createClass( {
 
-    getInitialState: function () {
+    getInitialState: function() {
         return {
             isEditing: false,
             constituency: this.props.constituency,
@@ -10,50 +10,50 @@ var SingleConstituencyContainer = React.createClass({
     },
 
 
-    handleFieldChange: function (fieldName) {
-        return function (constituency) {
+    handleFieldChange: function( fieldName ) {
+        return function( constituency ) {
             var tempConstituency = this.state.constituency;
             tempConstituency[fieldName] = constituency.target.value;
-            if (!this.state.isChanged) {
-                this.setState({fieldContainer: constituency.target.value})
+            if ( !this.state.isChanged ) {
+                this.setState( { fieldContainer: constituency.target.value })
             }
-            this.setState({constituency: tempConstituency, isChanged: true});
-        }.bind(this);
+            this.setState( { constituency: tempConstituency, isChanged: true });
+        }.bind( this );
     },
 
-    handleEditConst: function (constitut) {
+    handleEditConst: function( constitut ) {
 
-        if (this.state.isEditing) {
-            this.setState({isEditing: false})
+        if ( this.state.isEditing ) {
+            this.setState( { isEditing: false })
             this.state.constituency.title = this.state.fieldContainer;
             this.forceUpdate()
         } else {
-            this.setState({isEditing: true})
+            this.setState( { isEditing: true })
         }
 
     },
 
-    handleSaveConst: function () {
-        
+    handleSaveConst: function() {
         var name = $( "#ConstituencyName" ).val();
         var matches = name.match( ".*([a-zA-Z0-9ąčęėįšųūžĄČĘĖĮŠŲŪŽ„“]$)" );
-        if ( matches != null ) { $( '#ConstituencyNameValidation' ).hide( "slow" ) }
-        else { $( '#ConstituencyNameValidation' ).hide( "slow" ); $( '#ConstituencyNameValidation' ).show( "slow" ) }
-        
-//        axios.post('/api/constituencies', this.state.constituency)
-//            .then(this.setState({isEditing: false}))
+        if ( matches != null ) {
+            $( '#ConstituencyNameValidation' ).hide( "slow" );
+            axios.post( '/api/constituencies', this.state.constituency )
+                .then( this.setState( { isEditing: false }) )
+        }
+        else { $( '#ConstituencyNameValidation' ).hide( "slow" ); $( '#ConstituencyNameValidation' ).show( "slow" ) }       
     },
 
-    render: function () {
+    render: function() {
         return (
             <SingleConstituencyComponent constituency={this.props.constituency}
-                                         onEditDistrict={this.props.onEditDistrict}
-                                         onEditConst={this.handleEditConst}
-                                         onDeleteConst={this.props.onDeleteConst}
-                                         onSaveConst={this.handleSaveConst}
-                                         isEditing={this.state.isEditing}
-                                         onCandidates={this.props.onCandidates}
-                                         onFieldChange={this.handleFieldChange}/>
+                onEditDistrict={this.props.onEditDistrict}
+                onEditConst={this.handleEditConst}
+                onDeleteConst={this.props.onDeleteConst}
+                onSaveConst={this.handleSaveConst}
+                isEditing={this.state.isEditing}
+                onCandidates={this.props.onCandidates}
+                onFieldChange={this.handleFieldChange} />
         );
 
 
