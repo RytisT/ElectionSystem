94d21683e5ddac34e82669a83e705c2d89f022bb
Edvardas <ahadvardas@gmail.com>
2017-03-07 13:08:05 +0200

    added footer

--- election-system/src/main/resources/static/js/components/App.jsx
+++ election-system/src/main/resources/static/js/components/App.jsx
@@ -8,7 +8,7 @@ var App = React.createClass({
     render: function () {
         return (
             <div>
-                <h1 className="text-center">Seimo Rinkimai</h1>
+                <h1 className="text-center" id="headerLogo">Seimo Rinkimai</h1>
                 <ul className="header">
                     <li id="home"><IndexLink to="/" className="glyphicon glyphicon-home"></IndexLink></li>
                     <li id="login"><Link to="/login" activeClassName="active">Administratorius</Link></li>
@@ -16,6 +16,17 @@ var App = React.createClass({
                 </ul>
                 <div className="content">
                     {this.props.children}
+                </div>
+                <div className="footer-bottom">
+                	<div className="container">
+                		<div className="row">
+                			<div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                				<div className="copyright">
+                					© 2017, CodeCamp
+                				</div>
+                			</div>
+                		</div>
+                	</div>
                 </div>
             </div>
         );
