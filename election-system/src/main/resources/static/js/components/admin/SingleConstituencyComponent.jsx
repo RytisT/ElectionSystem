ffea22b49527ff97debd32db906feb90dd1ecd9b
Diktas <g.simanelis@gmail.com>
2017-03-14 15:35:24 +0200

    Confirms on deletions

--- election-system/src/main/resources/static/js/components/admin/SingleConstituencyComponent.jsx
+++ election-system/src/main/resources/static/js/components/admin/SingleConstituencyComponent.jsx
@@ -1,37 +1,50 @@
+var styles = {
+    hidden: {
+        display: 'none'
+    },
+    width: {
+        width: '100px'
+    },
+    space: {
+        marginLeft: '2px'
+    },
+};
+
 var SingleConstituencyComponent = React.createClass( {
 
     render: function() {
         return this.props.isEditing
             ? <tr>
-                <td className="col-md-5"><input id="ConstituencyName" className="form-control" size="3"
-                                                placeholder="Įveskite Apygardos pavadinimą"
-                                                value={this.props.constituency.title}
-                                                onChange={this.props.onFieldChange( 'title' )} type="text" /><br />
+                <td className="col-md-6"><input id="ConstituencyName" className="form-control" size="3"
+                    placeholder="Įveskite Apygardos pavadinimą"
+                    value={this.props.constituency.title}
+                    onChange={this.props.onFieldChange( 'title' )} type="text" /><br />
                     <div id="ConstituencyNameValidation" className="validationForm">
                         <span> naudojami netinkami simboliai. </span></div></td>
                 <td className="col-md-2"></td>
-                <td className="col-md-2"></td>
+                <td className="col-md-1"></td>
                 <td className="col-md-2">
                     <button id="Update_constituency" type="button" className="btn btn-success" onClick={this.props.onSaveConst}>Išsaugoti
                     </button>
-                    <button id="Cancel_update" type="button" className="btn btn-danger" onClick={this.props.onEditConst}>Atšaukti</button>
+                    <button id="Cancel_update" type="button" className="btn btn-danger" style={styles.space}
+                        onClick={this.props.onEditConst}>Atšaukti</button>
                 </td>
                 <td className="col-md-1">
                     <button id="Delete_Constituency" type="button" className="btn btn-danger"
-                            onClick={this.props.onDeleteConst( this.props.constituency )}>Trinti
+                        onClick={this.props.onDeleteConst( this.props.constituency )}>Trinti
                     </button>
                 </td>
             </tr>
-            : <tr onMouseOver={this.props.onHover()} onMouseOut={this.props.onHover()} style={{backgroundColor: this.props.color}}>
-                <td className="col-md-5">{this.props.constituency.title}</td>
+            : <tr onMouseOver={this.props.onHover()} onMouseOut={this.props.onHover()} style={{ backgroundColor: this.props.color }}>
+                <td className="col-md-6">{this.props.constituency.title}</td>
                 <td className="col-md-2">
                     <button id="Edit_Districts" type="button" className="btn btn-info"
-                            onClick={this.props.onEditDistrict( this.props.constituency )}>Redaguoti apylinkes
+                        onClick={this.props.onEditDistrict( this.props.constituency )}>Redaguoti apylinkes
                     </button>
                 </td>
-                <td className="col-md-2">
+                <td className="col-md-1">
                     <button id="Candidates_info" type="button" className="btn btn-info"
-                            onClick={this.props.onCandidates( this.props.constituency )}>Kandidatai
+                        onClick={this.props.onCandidates( this.props.constituency )}>Sąrašas
                     </button>
                 </td>
                 <td className="col-md-2">
@@ -40,8 +53,17 @@ var SingleConstituencyComponent = React.createClass( {
                     </button>
                 </td>
                 <td className="col-md-1">
-                    <button id="Delete_constituency" type="button" className="btn btn-danger"
-                            onClick={this.props.onDeleteConst( this.props.constituency )}>Trinti
+                    <button id={"x_constituency" + this.props.constituency.id} type="button" className="btn btn-danger"
+                        onClick={( event ) => {
+                            var x = this.props.constituency.id;
+                            var y = "#Delete_constituency" + x;
+                            var z = "#x_constituency" + x
+                            if ( $( y ).is( ":hidden" ) ) { $( y ).show(); $( z ).html( "Atšaukti" ) }
+                            else { $( y ).hide(); $( z ).html( "Trinti" ); }
+                        } }>Trinti
+                    </button>
+                    <button id={"Delete_constituency" + this.props.constituency.id} style={styles.hidden} type="button" className="btn btn-danger"
+                        onClick={this.props.onDeleteConst( this.props.constituency )}>Patvirtinti
                     </button>
                 </td>
             </tr>
