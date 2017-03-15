6f47cd637a63a592e41905b838bbd525240e5488
Dainius D <ddiciunas@gmail.com>
2017-03-15 12:31:47 +0200

    fix for fix

--- election-system/src/main/java/lt/itakademija/security/SecurityWebAppInitializer.java
+++ election-system/src/main/java/lt/itakademija/security/SecurityWebAppInitializer.java
@@ -1,5 +1,5 @@
-package lt.itakademija.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer{}
+//package lt.itakademija.security;
+//
+//import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+//
+//public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer{}
