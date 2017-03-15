ae88a07fcfa48d0da4439a5de0bd09b78a9bf281
Dainius D <ddiciunas@gmail.com>
2017-03-15 11:40:00 +0200

    districts sibmit time

--- election-system/src/main/resources/static/js/components/results/ResultsDistrictsComponent.jsx
+++ election-system/src/main/resources/static/js/components/results/ResultsDistrictsComponent.jsx
@@ -1,6 +1,47 @@
 var ResultsDistrictsComponent = React.createClass({
 
     render: function () {
+        var timeCount = function(district) {
+            var votedSingleDate = new Date(district.votedSingleTime);
+            var year = votedSingleDate.getFullYear();
+            var month = votedSingleDate.getMonth() + 1;
+            var date = votedSingleDate.getDate();
+            if (votedSingleDate.getHours() < 10) {
+                var hour = "0" + votedSingleDate.getHours();
+            } else {
+                var hour = votedSingleDate.getHours();
+            }
+            var minutes = "0" + votedSingleDate.getMinutes();
+            var seconds = "0" + votedSingleDate.getSeconds();
+            if (month < 10) {
+                month = '0' + month;
+            }
+            if (date < 10) {
+                date = '0' + date;
+            }
+            var singleDate = year + '-' + month + '-' + date + "    " + hour + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+            var votedMultiDate = new Date(district.votedMultiTime);
+            var year = votedMultiDate.getFullYear();
+            var month = votedMultiDate.getMonth() + 1;
+            var date = votedMultiDate.getDate();
+            if (votedMultiDate.getHours() < 10) {
+                var hour = "0" + votedMultiDate.getHours();
+            } else {
+                var hour = votedMultiDate.getHours();
+            }
+            var minutes = "0" + votedMultiDate.getMinutes();
+            var seconds = "0" + votedMultiDate.getSeconds();
+            if (month < 10) {
+                month = '0' + month;
+            }
+            if (date < 10) {
+                date = '0' + date;
+            }
+            var multiDate = year + '-' + month + '-' + date + " " + hour + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+            return {"singleDate": singleDate, "multiDate": multiDate};
+        }.bind(this);
+
         var progress = function () {
             var submittedDistrict = 0;
             this.props.districts.map(function (district, index){
@@ -12,38 +53,41 @@ var ResultsDistrictsComponent = React.createClass({
         }.bind(this);
 
         var districtsList = this.props.districts.map(function (district, index) {
+            var timeCounter = timeCount(district);
             return (
                 <tr key={index}>
-                    <td className="col-md-1">{district.id}</td>
+                    <td className="col-md-">{district.id}</td>
                     <td className="col-md-3">{district.title}</td>
-                    <td className="col-md-2">
+                    <td className="col-md-3">{timeCounter.singleDate}</td>
+                    <td className="col-md-2">{timeCounter.multiDate}</td>
+                    <td className="col-md-1">
                         <button id={"ResultsDistrictsInfo"+district.id} type="button" className="btn btn-info"
                                 onClick={this.props.onResultsDistrictsInfoClick(district)}
                                 data-toggle="tooltip" data-placement="top" title="Apylinkės rezultatai"
                                 data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true"
                                 aria-controls="collapseOne"
                         >
-                            Balsavimas apylinkėje
+                            Apylinkės rez.
                         </button>
                     </td>
-                    <td className="col-md-2">
+                    <td className="col-md-1">
                         <button id={"ResultsDistrictsMulti"+district.id} type="button" className="btn btn-warning"
                                 onClick={this.props.onResultsDistrictsMultiClick(district)}
                                 data-toggle="tooltip" data-placement="top" title="Daugiamandatės rezultatai"
                                 data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true"
                                 aria-controls="collapseOne"
                         >
-                            Partijų rezultatai
+                            Partijų rez.
                         </button>
                     </td>
-                    <td className="col-md-2">
+                    <td className="col-md-1">
                         <button id={"ResultsDistrictsSingle"+district.id} type="button" className="btn btn-danger"
                                 onClick={this.props.onResultsDistrictsSingleClick(district)}
                                 data-toggle="tooltip" data-placement="top" title="Vienmandatės rezultatai"
                                 data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true"
                                 aria-controls="collapseOne"
                         >
-                            Kandidatų rezultatai
+                            Kandidatų rez.
                         </button>
                     </td>
                 </tr>
@@ -70,6 +114,8 @@ var ResultsDistrictsComponent = React.createClass({
                     <tr>
                         <th>Nr.</th>
                         <th>Apylinkės</th>
+                        <th>Vienmandatės duomenų laikas</th>
+                        <th>Daugiamandatės duomenų laikas</th>
                         <th>Rezultatai</th>
                         <th>Daugiamandatė</th>
                         <th>Vienmandatė</th>
